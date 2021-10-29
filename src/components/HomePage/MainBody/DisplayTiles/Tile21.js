1af95b725e6a5cafa7c6e6dfa636c718a4f080f4
Jefferson <jeffmonteiro61@icloud.com>
2021-10-28 23:07:07 -0300

    HomePage

--- src/components/HomePage/MainBody/DisplayTiles/Tile21.js
+++ src/components/HomePage/MainBody/DisplayTiles/Tile21.js
@@ -11,18 +11,6 @@ function Tile() {
                         <li>React Router helps you create routes to your single page applications. It’s very powerful and easy to use with your React application.</li>
                         </ul>
                     </h4>
-                    <section className="sectionMain">
-                        <div className = "fPlan">
-                            <h2>
-                                <b className="Text-Box">Plano Mensal</b>
-                            </h2>
-                        </div>
-                        <div className = "sPlan">
-                            <h2>
-                                Plano Anual
-                            </h2>
-                        </div>
-                    </section>
                 </div>
             </div>
 
