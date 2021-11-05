e79844f1f174920bef4d6b4a1351e28a2aa32f4d
Rafaela Gomes <rafaela.nvg@gmail.com>
2021-11-04 23:58:47 -0300

    Algumas alterações

--- src/components/Society/Components/facultyList.js
+++ src/components/Society/Components/facultyList.js
@@ -1,9 +1,8 @@
-
 const facultyList = [
     {
-        image: 'https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/gallery/5e4c114981ae6_images__1_.png?d=600x600',
-        name: 'The Debating Society of NSUT',
-        description: 'The debating society of NSIT is a promoter of discussion and deliberation, and focuses on providing people a platform for shedding their inhibitions and learning the art of expressing. It is an open society which primarily means one doesn\'t have to be a member to take part in society\'s activities. No prerequisite is demanded from participants, just the will to get involved and speak.'
+        image: 'https://images.vexels.com/media/users/3/131746/isolated/preview/2e0f322e515b5bd6d5fa6f383590f1f8-silhueta-de-menina-pilates.png?d=600x600',
+        name: 'Contato',
+        description: 'Fale conosco. Fique a vontade para entrar em contato.'
     }
 ]
 
