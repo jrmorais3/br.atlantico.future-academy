5cc1e0e42d659420a27b4fe5709f4bae96514224
Rafaela Gomes <rafaela.nvg@gmail.com>
2021-11-06 01:18:56 -0300

    .

--- src/components/forms/ContatoForm.jsx
+++ src/components/forms/ContatoForm.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Form, Button, Message} from 'semantic-ui-react';
 import InlineError from '../misc/InlineError';
-import './LoginForm.css';
+import './ContatoForm.css';
 
 class ContatoForm extends Component{
     constructor(props){
