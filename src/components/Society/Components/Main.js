17830ca4091cca8d0582601636bea2560aaf0d0c
Estêfenis Freitas Lopes <lopefl89@gmail.com>
2021-11-06 00:08:10 -0300

    update form

--- src/components/Society/Components/Main.js
+++ src/components/Society/Components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import facultyList from './facultyList';
 import FacultyCard from './FacultyCard';
+import ContatoForm from '../../forms/ContatoForm';
 
 class SocietyMain extends React.Component{
     constructor(){
@@ -13,16 +14,17 @@ class SocietyMain extends React.Component{
     {
         const facultyData = this.state.data.map(item => <FacultyCard key={item.name} item={item}/>)
         return (
-            <main>
+            <main className='Society-main'>
                 <div className='Society-Heading'>
                     <hr />
-                    <h2>Societies</h2>
+                    <h2>Contato</h2>
                     <hr />
                 </div>
                 
                 <div className='Society-Context'>
                     <div className='Society-Content'>
                         {facultyData}
+                        <ContatoForm submit={this.submit}/><br/>
                     </div>
                 </div>
             </main>
--- src/components/Society/Components/facultyList.js
+++ src/components/Society/Components/facultyList.js
@@ -1,8 +1,7 @@
 const facultyList = [
     {
-        image: 'https://images.vexels.com/media/users/3/131746/isolated/preview/2e0f322e515b5bd6d5fa6f383590f1f8-silhueta-de-menina-pilates.png?d=600x600',
-        name: 'Contato',
-        description: 'Fale conosco. Fique a vontade para entrar em contato.'
+        name: '',
+        description: ''
     }
 ]
 
